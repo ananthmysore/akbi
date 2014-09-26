0d499ea99dd867d7a5022c5efce5da1492270d3e
Ananth <ananthj87@gmail.com>
2014-09-26 16:53:11 +0530

    Initiation

---
+++ php/js/common.js
@@ -0,0 +1,11 @@
+function openMenu()
+{
+	if(document.getElementById("menu").style.left == "0px")
+	{
+		document.getElementById("menu").style.left="-210px";
+	}
+	else
+	{
+		document.getElementById("menu").style.left="0px";
+	}	
+}
